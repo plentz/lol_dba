0c71c80b18259cf54d541e5f956f195ec27d49cf
Diego Plentz <diego@plentz.org>
2012-12-21 19:27:12 -0200

    better and working tests. fixes #3

--- spec/associations_index_spec.rb
+++ spec/associations_index_spec.rb
@@ -2,77 +2,76 @@ require 'spec_helper'
 
 describe "Collect indexes based on associations:" do
 
-  before(:all) do
-    @relationship_indexes, @warning_messages = LolDba.check_for_indexes
-  end
+  let!(:lol_dba){ LolDba.check_for_indexes }
+  let(:relationship_indexes){ lol_dba[0] }
+  let(:warning_messages){ lol_dba[1] }
 
   it "find relationship indexes" do
-    @relationship_indexes.should_not be_empty
+    relationship_indexes.should_not be_empty
 
-    @relationship_indexes.should have_key("companies")
-    @relationship_indexes.should have_key("companies_freelancers")
-    @relationship_indexes.should have_key("addresses")
-    @relationship_indexes.should have_key("purchases")
+    relationship_indexes.should have_key("companies")
+    relationship_indexes.should have_key("companies_freelancers")
+    relationship_indexes.should have_key("addresses")
+    relationship_indexes.should have_key("purchases")
   end
 
   it "find indexes for belongs_to" do
-    @relationship_indexes["addresses"].should include("country_id")
+    relationship_indexes["addresses"].should include("country_id")
   end
 
   it "find indexes for belongs_to with custom foreign key" do
-    @relationship_indexes["companies"].should include("owner_id")
+    relationship_indexes["companies"].should include("owner_id")
   end
 
   it "find indexes for has_and_belongs_to_many" do
-    @relationship_indexes["companies_freelancers"].should include(["freelancer_id", "company_id"])
+    relationship_indexes["companies_freelancers"].should include(["freelancer_id", "company_id"])
   end
 
   it "find indexes for has_and_belongs_to_many with custom join_table, primary and foreign keys" do
-    @relationship_indexes["purchases"].should include(["present_id", "buyer_id"])
+    relationship_indexes["purchases"].should include(["present_id", "buyer_id"])
   end
 
   it "find two combinations for the combined index for has_and_belongs_to_many" do
-    @relationship_indexes["companies_freelancers"].should include(["freelancer_id", "company_id"])
-    @relationship_indexes["purchases"].should include(["present_id", "buyer_id"])
-    @relationship_indexes["purchases"].should include(["buyer_id", "present_id"])
+    relationship_indexes["companies_freelancers"].should include(["freelancer_id", "company_id"])
+    relationship_indexes["purchases"].should include(["present_id", "buyer_id"])
+    relationship_indexes["purchases"].should include(["buyer_id", "present_id"])
   end
 
   it "do not add an already existing index" do
-    @relationship_indexes["companies"].should_not include("country_id")
+    relationship_indexes["companies"].should_not include("country_id")
   end
 
   it "find indexes for has_many :through" do
-    puts @relationship_indexes["billable_weeks"].inspect
-    @relationship_indexes["billable_weeks"].should include(["remote_worker_id", "timesheet_id"])
-    @relationship_indexes["complex_billable_week"].should include(["freelancer_id", "id_complex_timesheet"])
+    relationship_indexes["billable_weeks"].should include(["remote_worker_id", "timesheet_id"])
+    relationship_indexes["complex_billable_week"].should include(["freelancer_id", "id_complex_timesheet"])
   end
 
   it "find indexes for has_many :through with source and foreign key" do
-    @relationship_indexes["complex_billable_week"].should include(["freelancer_id", "id_complex_timesheet"])
+    relationship_indexes["complex_billable_week"].should include(["freelancer_id", "id_complex_timesheet"])
   end
 
   it "do not include wrong class" do
-    @relationship_indexes["wrongs"].should be_nil
-    @relationship_indexes["addresses_wrongs"].should be_nil
+    relationship_indexes["wrongs"].should be_nil
+    relationship_indexes["addresses_wrongs"].should be_nil
   end
 
   it "have warnings(non-existent table) on test data" do
-    @warning_messages.should_not be_empty
-    @warning_messages.should =~ /\'wrongs\'/
-    @warning_messages.should =~ /\'addresses_wrongs\'/
+    warning_messages.should_not be_empty
+    warning_messages.should =~ /\'wrongs\'/
+    warning_messages.should =~ /\'addresses_wrongs\'/
   end
 
   it "find indexes for STI" do
-    @relationship_indexes["users"].should include(["id", "type"])
+    relationship_indexes["users"].should include(["id", "type"])
   end
 
   it "find indexes for STI with custom inheritance column" do
-    @relationship_indexes["freelancers"].should include(["id", "worker_type"])
+    relationship_indexes["freelancers"].should include(["id", "worker_type"])
   end
 
   it "find indexes, than use custom class name option in association" do
-    @relationship_indexes["employers_freelancers"].should be_nil
-    @relationship_indexes["companies_freelancers"].should include(["freelancer_id", "company_id"])
+    relationship_indexes["employers_freelancers"].should be_nil
+    relationship_indexes["companies_freelancers"].should include(["freelancer_id", "company_id"])
   end
 
 end
