54cf692c25a8a694819952aeccf677d88450f824
Christopher R. Murphy <christopher.roy.murphy@gmail.com>
2017-04-04 22:20:59 -0400

    Fix incorrect output of missing indexes (#84)
    Fixes Issue #83.

--- lib/lol_dba.rb
+++ lib/lol_dba.rb
@@ -171,13 +171,13 @@ EOM
 
   def self.puts_migration_content(migration_name, indexes, warning_messages)
     puts warning_messages
-    if indexes.keys.empty?
+    add = form_data_for_migration(indexes)
+    if add.blank?
       puts "Yey, no missing indexes found!"
     else
       tip = "* TIP: if you have a problem with the index name('index name too long') you can solve with the :name option. "
       tip += "Something like :name => 'my_index'."
       puts tip
-      add = form_data_for_migration(indexes)
       puts form_migration_content(migration_name, add)
     end
   end
