a15848e44d7742dd0a3a0e4faf52daeb67864d3f
Chris Mason <chris@chaione.com>
2018-05-02 13:31:09 -0500

    Create migrate_sql directory
    Needs to be present in order to run migration tests.

--- spec/sql_generator_spec.rb
+++ spec/sql_generator_spec.rb
@@ -3,6 +3,10 @@ require 'lol_dba/sql_generator'
 
 RSpec.describe "Sql Generator migrations:" do
 
+  before do
+    FileUtils.mkdir_p(Pathname.new(Rails.root).join('db', 'migrate_sql'))
+  end
+
   it "generates migrations without error" do
     expect { LolDba::SqlGenerator.generate('all') }.not_to raise_error
   end
