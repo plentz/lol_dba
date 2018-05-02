1501a34a799b3f9be744e73cdf92517e90ee4865
Chris Mason <chris@chaione.com>
2018-05-02 13:48:16 -0500

    Improve readability and reduce method complexity

--- lib/lol_dba/sql_generator.rb
+++ lib/lol_dba/sql_generator.rb
@@ -101,19 +101,32 @@ module LolDba
       end
 
       def migrator
-          ActiveRecord::Migrator.new(:up, migrations_path)
+        ActiveRecord::Migrator.new(:up, migrations_path)
       end
 
       def migrations_path
-        if Gem::Version.new(ActiveRecord::VERSION::STRING) >= Gem::Version.new('5.2.0')
-          ActiveRecord::MigrationContext.new(ActiveRecord::Migrator.migrations_paths).migrations
-        elsif ::ActiveRecord::VERSION::MAJOR == 5
-          ActiveRecord::Migrator.migrations(ActiveRecord::Migrator.migrations_paths)
-        elsif ::ActiveRecord::VERSION::MAJOR == 4
-          ActiveRecord::Migrator.migrations(ActiveRecord::Migrator.migrations_path)
+        ar_version = Gem::Version.new(ActiveRecord::VERSION::STRING)
+        if ar_version >= Gem::Version.new('5.2.0')
+          ar_5_2_0_migrations_path
+        elsif ar_version >= Gem::Version.new('5.0.0')
+          ar_5_0_0_migrations_path
+        elsif ar_version >= Gem::Version.new('4.0.0')
+          ar_4_0_0_migrations_path
         else
           ActiveRecord::Migrator.migrations_path
         end
+      end
+
+      def ar_5_2_0_migrations_path
+        ActiveRecord::MigrationContext.new(ActiveRecord::Migrator.migrations_paths).migrations
+      end
+
+      def ar_5_0_0_migrations_path
+        ActiveRecord::Migrator.migrations(ActiveRecord::Migrator.migrations_paths)
+      end
+
+      def ar_4_0_0_migrations_path
+        ActiveRecord::Migrator.migrations(ActiveRecord::Migrator.migrations_path)
       end
     end
   end
