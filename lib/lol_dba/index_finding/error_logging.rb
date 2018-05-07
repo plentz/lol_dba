12732ba8bc3f638c40a825e1d49b9e0639977d6e
Diego Plentz <diego@plentz.org>
2018-05-07 11:07:11 -0300

    changing log format a bit

--- lib/lol_dba/index_finding/error_logging.rb
+++ lib/lol_dba/index_finding/error_logging.rb
@@ -1,13 +1,14 @@
 module LolDba
   class ErrorLogging
     def self.log(class_name, reflection_options, exception)
-      puts 'Some errors here:'
-      puts 'Please, create an issue with the following information here https://github.com/plentz/lol_dba/issues:'
+      puts 'some errors here...'
+      puts 'please, create an issue with the following information here:'
+      puts 'https://github.com/plentz/lol_dba/issues'
       puts '***************************'
-      puts "Class: #{class_name}"
-      puts "Association type: #{reflection_options.macro}"
-      puts "Association options: #{reflection_options.options}"
-      puts "Exception: #{exception.message}"
+      puts "class: #{class_name}"
+      puts "association type: #{reflection_options.macro}"
+      puts "association options: #{reflection_options.options}"
+      puts "exception: #{exception.message}"
       exception.backtrace.each { |trace| puts trace }
     end
   end
