7a755578602cd6d1a8f9b3fd412efeff54b0aafc
Diego Plentz <diego@plentz.org>
2018-05-06 22:27:10 -0300

    extracting migration mocker class

--- lib/lol_dba.rb
+++ lib/lol_dba.rb
@@ -4,5 +4,6 @@ module LolDba
   require 'lol_dba/migration_formatter'
   require 'lol_dba/index_finder'
   require 'lol_dba/rails_compatibility'
+  require 'lol_dba/migration_mocker'
   require 'lol_dba/railtie.rb' if defined?(Rails)
 end
--- lib/lol_dba/sql_generator.rb
+++ lib/lol_dba/sql_generator.rb
@@ -9,61 +9,11 @@ module LolDba
 
       private
 
-      def connection
-        ActiveRecord::Base.connection
-      end
-
-      def methods_to_modify
-        %i[execute do_execute rename_column change_column column_for tables indexes select_all] & connection.methods
-      end
-
-      def redefine_execution_methods
-        save_original_methods
-        redefine_execute_methods(:execute)
-        # needed for activerecord-sqlserver-adapter
-        redefine_execute_methods(:do_execute)
-
-        connection.class.send(:define_method, :column_for) { |*args| args.last }
-        connection.class.send(:define_method, :change_column) { |*_args| [] }
-        connection.class.send(:define_method, :rename_column) { |*_args| [] }
-        connection.class.send(:define_method, :tables) { |*_args| [] }
-        connection.class.send(:define_method, :select_all) { |*_args| [] }
-        connection.class.send(:define_method, :indexes) { |*_args| [] }
-        # returns always the default(args[2])
-        connection.class.send(:define_method, :index_name_exists?) { |*args| args[2] }
-      end
-
-      def redefine_execute_methods(name)
-        connection.class.send(:define_method, name) do |*args|
-          if args.first =~ /SELECT "schema_migrations"."version"/ || args.first =~ /^SHOW/
-            orig_execute(*args)
-          else
-            Writer.write(to_sql(args.first, args.last))
-          end
-        end
-      end
-
-      def save_original_methods
-        methods_to_modify.each do |method_name|
-          connection.class.send(:alias_method, "orig_#{method_name}".to_sym, method_name)
-        end
-      end
-
-      def reset_methods
-        methods_to_modify.each do |method_name|
-          begin
-            connection.class.send(:alias_method, method_name, "orig_#{method_name}".to_sym)
-          rescue StandardError
-            nil
-          end
-        end
-      end
-
       def generate_instead_of_executing
         LolDba::Writer.reset
-        redefine_execution_methods
+        LolDba::MigrationMocker.redefine_execution_methods
         yield
-        reset_methods
+        LolDba::MigrationMocker.reset_methods
       end
 
       def migrations(which)
