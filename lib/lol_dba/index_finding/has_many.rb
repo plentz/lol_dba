e870a4d014895c21da30cc9de1d5b6be49e8f878
Diego Plentz <diego@plentz.org>
2018-05-08 10:33:26 -0300

    decreasing HasMany.relation_columns method complexity

--- lib/lol_dba/index_finding/has_many.rb
+++ lib/lol_dba/index_finding/has_many.rb
@@ -4,19 +4,11 @@ module LolDba
       # has_many tables are threaten by the other side of the relation
       return nil unless through && reflections[through.to_s]
 
-      through_reflections = through_class.reflections.stringify_keys
-      if (source = reflection_options.options[:source])
-        association_reflection = through_reflections[source.to_s]
-        return nil if association_reflection.options[:polymorphic]
-        association_foreign_key = get_through_foreign_key(association_reflection.klass, reflection_options)
-      elsif belongs_to_reflections = through_reflections[reflection_name.singularize]
-        # go to joining model through has_many and find belongs_to
-        association_foreign_key = belongs_to_reflections.options[:foreign_key]
+      # FIXME: currently we don't support :through =>
+      # :another_regular_has_many_and_non_through_relation
+      if (association_foreign_key = find_association_fk).present?
+        [association_foreign_key, foreign_key].map(&:to_s).sort
       end
-
-      # FIXME: currently we don't support :through => :another_regular_has_many_and_non_through_relation
-      return nil unless association_foreign_key.present?
-      [association_foreign_key, foreign_key].map(&:to_s).sort
     end
 
     def table_name
@@ -24,6 +16,17 @@ module LolDba
     end
 
     private
+
+    def find_association_fk
+      if (source = reflection_options.options[:source])
+        association_reflection = through_reflections[source.to_s]
+        return nil if association_reflection.options[:polymorphic]
+        get_through_foreign_key(association_reflection.klass, reflection_options)
+      elsif belongs_to_reflections = through_reflections[reflection_name.singularize]
+        # go to joining model through has_many and find belongs_to
+        belongs_to_reflections.options[:foreign_key]
+      end
+    end
 
     def through_class
       reflections[through.to_s].klass
@@ -35,6 +38,10 @@ module LolDba
 
     def foreign_key
       get_through_foreign_key(class_name, reflection_options)
+    end
+
+    def through_reflections
+      through_class.reflections.stringify_keys
     end
   end
 end
