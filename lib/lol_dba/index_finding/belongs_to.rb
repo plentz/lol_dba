cd078489e9bd698cee7671b4d71bbafa8d31a887
Diego Plentz <diego@plentz.org>
2018-05-08 09:14:28 -0300

    simplifying belongs_to class

--- lib/lol_dba/index_finding/belongs_to.rb
+++ lib/lol_dba/index_finding/belongs_to.rb
@@ -4,26 +4,28 @@ module LolDba
       if reflection_options.options[:polymorphic]
         poly_type = "#{reflection_options.name}_type"
         poly_id = "#{reflection_options.name}_id"
-        index_name = [poly_type, poly_id].sort
+        [poly_type, poly_id].sort
       else
-        foreign_key = reflection_options.options[:foreign_key]
-        foreign_key ||= if reflection_options.respond_to?(:primary_key_name)
-                          reflection_options.primary_key_name
-                        else
-                          reflection_options.foreign_key
-          end
+        foreign_key = non_polymorphic_fk
 
         # not a clue why rails 4.1+ creates this left_side_id thing
-        if foreign_key == 'left_side_id'
-          nil
-        else
-          index_name = foreign_key.to_s
-        end
+        foreign_key == 'left_side_id' ? nil : foreign_key.to_s
       end
     end
 
     def table_name
       class_name.table_name
+    end
+
+    private
+
+    def non_polymorphic_fk
+      foreign_key = reflection_options.options[:foreign_key]
+      foreign_key ||= if reflection_options.respond_to?(:primary_key_name)
+                        reflection_options.primary_key_name
+                      else
+                        reflection_options.foreign_key
+        end
     end
   end
 end
